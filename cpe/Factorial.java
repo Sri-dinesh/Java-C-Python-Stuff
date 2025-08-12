8f450b7d12b9b893a1d4ae8dd53512fe731321eb
Sri-dinesh <santhisridinesh@gmail.com>
2025-08-12 20:32:01 +0530

    12/8/25 - glimpse stringbuffer, stringBuilder

---
+++ cpe/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Factorial {
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int number = sc.nextInt();
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+        sc.close();
+    }
+}
