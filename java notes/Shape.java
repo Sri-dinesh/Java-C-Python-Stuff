e6e9b71071e8cb404b93f2192564a740a9548d32
Sri-dinesh <santhisridinesh@gmail.com>
2025-09-05 20:02:48 +0530

    java lab programs

---
+++ java notes/Shape.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+abstract class Shape {
+
+    double height;
+    double base;
+    
+    void setParam(double height, double base) {
+        this.height = height;
+        this.base = base;
+    }
+
+    abstract double shapeArea();
+    public void display() {
+        System.out.println("The height is: " + height + "& base is:" + base);
+    }
+}
+
+
+class Triange extends Shape{
+
+void shapeArea(){
+  System.out.println("Area of triangle:" + 0.5 * super.base * super.height);
+}
+
+}
+
+class Rectangle extends Shape {
+void shapeArea() {
+  system.out.println("Area of rectangle: " + super.base * super.height);
+}
+}
+
+
+public class ShapeDemo {
+
+  public static void main(String[] args) {
+
+  Scanner sc  = new Scanner(System.in);
+  System.out.println("Enter the base and height");
+  double base;
+  double height;
+  base = sc.nextDouble();
+  height = sc.nextDouble();
+
+  Rectangle r = new Rectangle();
+  r.setPara(base, height);
+
+  Triangle t = new Triangle();
+  t.setParam(base, height);
+
+
+  
+
+    
+  }
+
+}
